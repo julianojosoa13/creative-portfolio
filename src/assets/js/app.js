d191c2195176cb6ffae1a0e419c1f749360d3d17
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-05-12 23:41:06 +0300

    I made the animation responsive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,15 +2,18 @@ import gsap from "gsap";
 
 const bar = document.querySelector(".loading__bar--inner");
 const progress = document.querySelector(".loading__counter--number");
+
+const progressBox = document.querySelector(".loading__box");
+
 let c = 0;
 
 let barInterval = setInterval(() => {
   bar.style.width = c + "%";
   progress.innerText = c + "%";
+
   c++;
   if (c > 100) {
     clearInterval(barInterval);
-
     gsap.to(".loading__bar", {
       duration: 5,
       rotate: "90deg",
@@ -19,15 +22,17 @@ let barInterval = setInterval(() => {
     });
 
     gsap.to(".loading__text,.loading__counter", {
-      duration: 1,
+      duration: 0.5,
       opacity: 0,
     });
 
+    const currentWidth = progressBox.clientWidth;
+
     gsap.to(".loading__box", {
       duration: 1,
-      height: "500px",
+      height: currentWidth,
+      width: currentWidth,
       borderRadius: "50%",
-      borderColor: "#6cff8d",
     });
   }
 }, 20);
