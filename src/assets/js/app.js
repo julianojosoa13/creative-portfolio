a9a766cb58bf6b4aad815aa629a40f4cec0e89ad
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-05-12 22:40:13 +0300

    Making the percentage nunber grow

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,10 @@
 const bar = document.querySelector(".loading__bar--inner");
+const progress = document.querySelector(".loading__counter--number");
 let c = 0;
 
 let barInterval = setInterval(() => {
   bar.style.width = c + "%";
+  progress.innerHTML = c + "%";
   c++;
   if (c > 100) {
     clearInterval(barInterval);
