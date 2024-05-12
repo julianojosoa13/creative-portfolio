3c4feacc7abc5e1426bb3d0c2e01e8c965141e39
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-05-12 23:00:54 +0300

    The loading bar goes away after it completes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 const bar = document.querySelector(".loading__bar--inner");
 const progress = document.querySelector(".loading__counter--number");
 let c = 0;
@@ -8,5 +10,11 @@ let barInterval = setInterval(() => {
   c++;
   if (c > 100) {
     clearInterval(barInterval);
+
+    gsap.to(".loading__bar", {
+      duration: 5,
+      rotate: "90deg",
+      left: "1000%",
+    });
   }
 }, 20);
